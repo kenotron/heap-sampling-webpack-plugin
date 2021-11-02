f6415433d971caf1904872cd22921c55de60f310
Ken Chau <kchau@microsoft.com>
2021-11-02 15:16:46 -0700

    Adds peak memory checker

--- src/HeapSamplingPlugin.ts
+++ src/HeapSamplingPlugin.ts
@@ -5,11 +5,13 @@ import { promisify } from "util";
 import { mkdirpSync } from "webpack/lib/util/fs";
 
 export interface HeapSamplingPluginOptions {
+  checkPeakMemory?: boolean;
   outputPath?: string;
 }
 
 export class HeapSamplingPlugin {
   private session: inspector.Session;
+  private peakMemory = 0;
 
   constructor(private options: HeapSamplingPluginOptions) {
     this.session = new inspector.Session();
@@ -24,9 +26,29 @@ export class HeapSamplingPlugin {
     }
   }
 
+  initializeMemoryChecker() {
+    const peakMemoryInterval = setInterval(() => {
+      const memoryUsage = process.memoryUsage();
+      const heapTotal = memoryUsage.heapTotal;
+
+      if (heapTotal > this.peakMemory) {
+        this.peakMemory = heapTotal;
+      }
+    }, 1000);
+
+    return () => peakMemoryInterval.unref();
+  }
+
   apply(compiler: Compiler): void {
     const fs = compiler.intermediateFileSystem;
+    const logger = compiler.getInfrastructureLogger("heap-sampling-plugin");
     const writeFile = promisify(fs.writeFile);
+
+    let disposeMemoryChecker: () => void;
+
+    if (this.options.checkPeakMemory) {
+      disposeMemoryChecker = this.initializeMemoryChecker();
+    }
 
     if (!this.options.outputPath) {
       this.options.outputPath = compiler.options.output.path;
@@ -41,10 +63,10 @@ export class HeapSamplingPlugin {
 
     compiler.hooks.afterEmit.tapPromise("HeapSamplingPlugin", async (stats) => {
       const { outputPath } = this.options;
-      
+
       const heapProfilerStopSampling = promisify(this.session.post.bind(this.session, "HeapProfiler.stopSampling"));
       const heapProfilerDisable = promisify(this.session.post.bind(this.session, "HeapProfiler.disable"));
-      
+
       const { profile } = await heapProfilerStopSampling();
       await heapProfilerDisable();
 
@@ -54,6 +76,11 @@ export class HeapSamplingPlugin {
       }
 
       await writeFile(this.options.outputPath, JSON.stringify(profile));
+
+      if (this.options.checkPeakMemory) {
+        logger.info(`Max memory usaged for webpack: ${(this.peakMemory / 1024 / 1024).toFixed(2)}MB`);
+        disposeMemoryChecker();
+      }
     });
   }
 }
